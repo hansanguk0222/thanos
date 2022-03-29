16fd397fcbc7a5d4e719cedce3d44422d15d7c40
Hansanguk <hansanguk0222@gmail.com>
2022-03-29 23:27:50 +0900

    [refactor] 오버 스펙이라고 생각되는 것들 모두 쳐냄

--- VanillaCarousel/src/components/Button/index.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@/components/Core/index';
-
-export class Button extends Component {
-  template() {
-    const { className, id, children } = this.props;
-    return `<button class=${className} id=${id}>${children}</button>`;
-  }
-}
