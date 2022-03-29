9fec2573df13bed3427cd3fdf9db604f7c8122bb
Hansanguk <hansanguk0222@gmail.com>
2022-03-29 23:35:50 +0900

    [feat] 모든 컴포넌트에서 상속해서 사용할 클래스를  제작

---
+++ VanillaCarousel/src/core/Component.js
@@ -0,0 +1,39 @@
+import { observable, observe } from "./observer.js";
+
+export class Component {
+  state;
+  props;
+  $el;
+
+  constructor($el, props) {
+    this.$el = $el;
+    this.props = props;
+    this.setup();
+  }
+
+  setup() {
+    this.render();
+    this.state = observable(this.initState());
+    observe(() => {
+      this.render();
+      this.setEvent();
+      this.mounted();
+      this.setCss();
+      this.firstDrawCanvas();
+    });
+  }
+
+  initState() {
+    return {};
+  }
+  template() {
+    return "";
+  }
+  render() {
+    this.$el.innerHTML = this.template();
+  }
+  setEvent() {}
+  mounted() {}
+  setCss() {}
+  firstDrawCanvas() {}
+}
